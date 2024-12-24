23f92398cfc7e081a2a6ca19ae33bf38e5356e1c
ThuktenSingye <thuktensingye2163@gmail.com>
2024-12-24 17:44:58 +0600

    #Hotwire#turbo#frame#stream

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get "habits/show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :habits, only: [:show] do
+    member do
+      post :plus
+      post :minus
+    end
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
